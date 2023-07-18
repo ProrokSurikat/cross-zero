941558d3fa33b8a5baf69b19c56cd9a0db3d5273
ProrokSurikat <guzovpasha@mail.ru>
2023-07-18 16:21:41 +0300

    aaaaa

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+arr = [['-', '-', '-'],
+     ['-', '-', '-'],
+     ['-', '-', '-']]
+def matrix(arr: list[list[str]], s: str, i: int, j: int):
+    arr[i][j] = s
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def move(arr: list[list[str]]):
+    is_x_turn = True
+    for i in arr:
+        for j in i:
+            print(f'Игрок {"X" if is_x_turn else "0"}, сделайте ход')
+            i = int(input('Введите стобец: '))
+            j = int(input('Введите строчку: '))
+            while arr[i][j] != '-':
+                print("Тут занято. Введите заново")
+                i = int(input('Введите стобец: '))
+                j = int(input('Введите строчку: '))
+            c = "X" if is_x_turn else "0"
+            matrix(arr, c, i, j)
+            print_matrix(arr)
+            if check_winner(arr,c):
+                print(f"Победил {c}")
+                return
+            is_x_turn = not is_x_turn
 
+def print_matrix(arr):
+    for i in arr:
+        print(i)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_winner(arr: list[list[str]], c: str):
+    if (arr[0][0] == c and arr[1][1] == c and arr[2][2] == c) or \
+            (arr[0][2] == c and arr[1][1] == c and arr[2][0] == c) or \
+             (arr[0][0] == c and arr[0][1] == c and arr[0][2] == c) or \
+              (arr[1][0] == c and arr[1][1] == c and arr[1][2] == c) or \
+               (arr[2][0] == c and arr[2][1] == c and arr[2][2] == c) or \
+                (arr[0][0] == c and arr[1][0] == c and arr[2][0] == c) or \
+                 (arr[0][1] == c and arr[1][1] == c and arr[2][1] == c) or \
+                  (arr[0][2] == c and arr[1][2] == c and arr[2][2] == c):
+        return True
+    else:
+        return False
 
+move(arr)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
